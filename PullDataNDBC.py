cf73116b7c13bb9e110914bc0e840e99c2c1a7e0
Tinker0425 <79473752+Tinker0425@users.noreply.github.com>
2021-02-22 12:49:30 -0900

    Update PullDataNDBC.py

--- PullDataNDBC.py
+++ PullDataNDBC.py
@@ -1,33 +1,4 @@
-import urllib.request  # the lib that handles the url stuff
-
-# Create a script that reads in the NDBC Buoy data and stores it
-# Then a second to analyze data
-
-# This is what I want because it creates an output array to use:
 import numpy as np
 data = np.loadtxt("https://www.ndbc.noaa.gov/view_text_file.php?filename=46085h2020.txt.gz&dir=data/historical/stdmet/", comments='#',skiprows=2)
 print(data)
 
-
-wspd_str = ''
-wspd_float = []
-gust_str = ''
-wave_str = ''
-
-k = 0
-
-for line in urllib.request.urlopen('https://www.ndbc.noaa.gov/view_text_file.php?filename=46085h2020.txt.gz&dir=data/historical/stdmet/'):
-    while 1 < k < 5 :
-        a = line.decode('utf-8')
-        wspd_str = a[21:25]
-        wspd_float.append(float(a[21:25]))
-        gust_str = a[26:31]   
-        wave_str = a[31:35]
-        print(wspd_float)
-    k += 1
-
-#wspd_float = wspd_float.*10
-#gust_str = a[26:31]   
-#wave_str = a[31:35]   
-
-print(wspd_float)
