02e3d4b3b61c63841996c980aed2c5ac1f02864a
Tinker0425 <79473752+Tinker0425@users.noreply.github.com>
2021-02-22 11:14:45 -0900

    Update PullDataNDBC.py

--- PullDataNDBC.py
+++ PullDataNDBC.py
@@ -1,8 +1,13 @@
 import urllib.request  # the lib that handles the url stuff
+wspd_str = ''
+wave_str = ''
 for line in urllib.request.urlopen('https://www.ndbc.noaa.gov/view_text_file.php?filename=46085h2020.txt.gz&dir=data/historical/stdmet/'):
     a = line.decode('utf-8')
-    wspd = a[21:26]
-    gust = a[26:31]   
-    wave = a[31:35]
-    
-  
+    wspd_str = wspd_str + a[21:25]
+    gust_str = a[26:31]   
+    wave_str = a[31:35]
+wspd_float = float(wspd_str[9:11])
+#gust_str = a[26:31]   
+#wave_str = a[31:35]   
+
+print(wspd_float)
