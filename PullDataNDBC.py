a5591e8c1a63faaaae9b467211644b7ccd9f6184
Tinker0425 <79473752+Tinker0425@users.noreply.github.com>
2021-02-22 10:30:11 -0900

    Update PullDataNDBC.py

--- PullDataNDBC.py
+++ PullDataNDBC.py
@@ -1,4 +1,9 @@
 import urllib.request  # the lib that handles the url stuff
 
 for line in urllib.request.urlopen('https://www.ndbc.noaa.gov/view_text_file.php?filename=46085h2020.txt.gz&dir=data/historical/stdmet/'):
-    print(line.decode('utf-8'))
+    a = line.decode('utf-8')
+    b = a[21:35]
+    #listWords = a.split("\t")
+    #print(listWords)
+    print(b)
+#print(a[0:5])
