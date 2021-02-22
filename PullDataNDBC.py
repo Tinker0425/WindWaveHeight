7a718e4b4b1ecddfbca3e170ede8b189acd296b1
Tinker0425 <79473752+Tinker0425@users.noreply.github.com>
2021-02-22 13:00:01 -0900

    Update PullDataNDBC.py

--- PullDataNDBC.py
+++ PullDataNDBC.py
@@ -1,4 +1,9 @@
 import numpy as np
-data = np.loadtxt("https://www.ndbc.noaa.gov/view_text_file.php?filename=46085h2020.txt.gz&dir=data/historical/stdmet/", comments='#',skiprows=2)
-print(data)
+Buoy_List = ["46085","46084","46083","46082","46205","46145"]
+Year_List = ["2013","2014","2015","2016","2017","2018","2019","2020"]
+for i in Buoy_List:
+    for j in Year_List:
+        data = np.loadtxt("https://www.ndbc.noaa.gov/view_text_file.php?filename="+i+"h"+j+".txt.gz&dir=data/historical/stdmet/", comments='#',skiprows=2)
+        print(i + ' ' + j)
+        
 
